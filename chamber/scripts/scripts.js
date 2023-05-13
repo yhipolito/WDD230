be21e44f05fdf24d18dcc8adf7e3625353cce284
yhipolito <yazelhipolito@gmail.com>
2023-05-13 13:29:57 +0000

    chamber file created

---
+++ chamber/scripts/scripts.js
@@ -0,0 +1,25 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerBtn")
+x.onclick = toggleMenu;
+
+// select the DOM elements to manipulate (we will output to these)
+const datefield = document.querySelector("time");
+// for european/family history format with day first.
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+// "full" or long, medium, short options ... try them
+
+datefield.textContent = fulldate;
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+datemessage.innerHTML = `Today is <strong>${fulldate}</strong>. What are you going to do?`;
