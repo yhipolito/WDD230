fd703246ea2bcfaf64f00cba1438036a8674624c
yhipolito <yazelhipolito@gmail.com>
2023-06-06 17:03:06 +0000

    updated last visit to miliseconds

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -64,3 +64,37 @@ if ("IntersectionObserver" in window) {
     loadImages(img);
   });
 }
+
+//initialize manipulation of DOM with visitDisplay which is <span id="lastVisti"></span>
+const visitsDisplay = document.querySelector("#lastVisit");
+// milliseconds to days constant = 1000 ms/s * 60 s/m * 60 m/h * 24 h/day
+const msToDays = 84600000;
+
+// Get the last visit date from local storage
+let lastVisit = localStorage.getItem("lastVisit");
+
+// If there was a last visit
+if (lastVisit) {
+  // Date.now() and lastVisit in miliseconds
+  // diffDays in days rounded.
+  let diffDays = Math.round((Date.now() - lastVisit) / msToDays);
+  //   console.log(diffDays);
+
+  // Display the result somewhere in the page
+  visitsDisplay.textContent = `It's been ${diffDays} days since your last visit.`;
+} else {
+  // If there's no last visit date in local storage, this is the user's first visit
+  visitsDisplay.textContent = "This is your first visit. Welcome!";
+}
+
+// Update the last visit date in local storage
+localStorage.setItem("lastVisit", Date.now());
+
+// Following lines show today's time in two different formats
+// Example Tue Jun 06 2023 16:54:53 GMT+0000 (Greenwich Mean Time)
+// from const now = new Date();
+// console.log(now);
+
+// Time in miliseconds elapsed since 1/1/1970
+// Date.now()
+// console.log(Date.now());
