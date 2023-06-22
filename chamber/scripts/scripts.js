9049de07da8da3b0014e9c99a58435f7a5dc0aff
yhipolito <yazelhipolito@gmail.com>
2023-06-22 21:04:06 +0000

    updates to weather section from home html

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -101,6 +101,43 @@ localStorage.setItem("lastVisit", Date.now());
 
 // formdate.textContent = now;
 
+///-----------------WEATHER INFO----------------///
+// select HTML elements in the document
+const currentTemp = document.querySelector('#temperature');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const urlw = "https://api.openweathermap.org/data/2.5/weather?q=Sandy,USA&appid=ac62c32e0537fc708a489b00c343f912&units=imperial"
+
+async function apiFetch(){
+    try {
+        const response = await fetch(urlw);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data); // this is for testing the call
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    }   catch(error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    // captionDesc.textContent = desc;
+    captionDesc.innerHTML = `<i>${desc}</i>`;
+}
+
 ///-------------DIRECTORY COMPANIES--------///
 
 const url = "https://yhipolito.github.io/WDD230/chamber/json/data.json";
