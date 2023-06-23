8d74603fd28e916c7880469a0ebc3eee2f639ed5
yhipolito <yazelhipolito@gmail.com>
2023-06-23 01:26:45 +0000

    gtag.js created

---
+++ chamber/scripts/gtag.js
@@ -0,0 +1,10 @@
+// <!-- Google tag (gtag.js) -->
+async;
+src = "https://www.googletagmanager.com/gtag/js?id=G-F5Z0EGFNLM";
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+
+gtag("config", "G-F5Z0EGFNLM");
